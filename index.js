3aa21579f362a09f45f05d4f4c1ab1e691bf9fff
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 20:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   var count = 0;
-  var lineString = "The line is currntly: ";
+  var lineString = "The line is currently: ";
 
   if (line.length === 0) {
     return "The line is currently empty.";
